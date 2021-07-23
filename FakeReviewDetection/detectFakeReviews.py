36820ec3901dd38681bb3f28512774093470c4ef
Christodoulos <chrisjab94@gmail.com>
2021-07-23 22:38:22 +0200

    First Experiment with datasets and models

--- FakeReviewDetection/detectFakeReviews.py
+++ FakeReviewDetection/detectFakeReviews.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
     print("Hub version: ", hub.__version__)
     print("GPU is", "available" if tf.config.list_physical_devices('GPU') else "NOT AVAILABLE")
 
-    train_df = pd.read_csv('Dataset/amazon_reviews.txt', index_col=False, delimiter="\t")
+    train_df = pd.read_csv('/Users/chrislyk/Documents/Coding/Applied DL in NLP - Seminar/SemesterProject/Natural-Language-Processing/FakeReviewDetection/Dataset/amazon_reviews.txt', index_col=False, delimiter="\t")
 
     #train_true_df = train_df [  train_df["LABEL"] == 0  ]
 
@@ -25,11 +25,11 @@ if __name__ == '__main__':
 
     train_shortened_df = train_df[features]
 
-    #print(train_shortened_df.head())
+    # print(train_shortened_df.head())
 
     review_array = train_shortened_df["REVIEW_TEXT"].to_numpy(dtype=object)
     labels_array = train_shortened_df["LABEL"].to_numpy(np.int32)
-    #print(review_array[0:1])
+    # print(review_array[0:1])
     # We first need to shuffle the data such that both training and validation dataset has both labels
     data_length = len(review_array)
     idx = np.random.permutation(data_length)
@@ -45,12 +45,12 @@ if __name__ == '__main__':
     y_val = y[train_data_len:]
     partial_y_train = y[:train_data_len]
 
-    #print(y_val)
-    #print(len(y_val == 0))
-    #print(len(y_val == 1))
+    # print(y_val)
+    # print(len(y_val == 0))
+    # print(len(y_val == 1))
 
-    #print(len(x_val == 0))
-    #print(len(x_val == 1))
+    # print(len(x_val == 0))
+    # print(len(x_val == 1))
 
 
     try:
@@ -62,7 +62,7 @@ if __name__ == '__main__':
     if not model_classifier:
         model = "https://tfhub.dev/google/nnlm-en-dim50/2"
         hub_layer = hub.KerasLayer(model, input_shape=[], dtype=tf.string, trainable=True)
-        #print( hub_layer(review_array[:3]) )
+        # print( hub_layer(review_array[:3]) )
 
 
         model_classifier = tf.keras.Sequential()
